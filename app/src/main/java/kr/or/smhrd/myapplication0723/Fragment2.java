06e2806581f633930041ebe88be0b6c5a5d0ad09
nhki01 <nhki01@naver.com>
2021-07-23 17:38:18 +0900

    안드로이드 앱 커밋

---
+++ app/src/main/java/kr/or/smhrd/myapplication0723/Fragment2.java
@@ -0,0 +1,23 @@
+package kr.or.smhrd.myapplication0723;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        TextView fg2_tv_data = view.findViewById(R.id.fg2_tv_data);
+        fg2_tv_data.setText("두 번째 프래그먼트 화면");
+
+        return view;
+    }
+}
